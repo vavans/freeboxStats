8791362e100a6eb7b1152de4773077c9aed5d1ce
vavans <vannnns@gmail.com>
2017-09-30 19:03:40 +0200

    Add freebox authorization and session

--- Program.fs
+++ Program.fs
@@ -1,5 +1,4 @@
 ﻿open System
-
 open System.IO
 open FreeBoxAPI
 
@@ -9,7 +8,7 @@ type Key = string
 type UnknownState = UnknownCon
 
 type KnownState = {
-    Key : Key
+    App_token : Key
 }
 
 type ConnectedState = {
@@ -21,16 +20,22 @@ type FreeDevice =
     | Known of KnownState
     | Connected of ConnectedState
 
-let getKey =
+let getKey : string option =
     if File.Exists("./key.txt") then
         Some (File.OpenText("./key.txt").ReadToEnd())
     else
         None
 
+let setKey (k : string) : unit =
+    if File.Exists("./key.txt") then
+        File.Delete("./key.txt")
+    use f = new StreamWriter("./key.txt")
+    f.Write(k)
+
 let initDevice =
     let key = getKey
     match key with
-    | Some k -> FreeDevice.Known { Key = k }
+    | Some k -> FreeDevice.Known { App_token = k }
     | None   -> FreeDevice.Unknown UnknownCon
 
 type UnknownState with
@@ -41,7 +46,14 @@ type UnknownState with
             printfn "Token reçu: %s" rep.result.app_token
             printfn "Appuyer sur entrée après avoir validé..."
             Console.ReadLine() |> ignore
-            Some rep.result.app_token
+            printfn "Vérification authorization"
+            let r = FreeBoxAPI.sendCheckStatus rep.result.track_id
+            if r.success then
+                printfn "Authorization status : %s" r.result.status
+                setKey rep.result.app_token
+                Some (FreeDevice.Known { App_token = rep.result.app_token })
+            else
+                None
         else
             None
 
@@ -50,52 +62,56 @@ let startDevice =
     let d = initDevice
     match d with
     | Unknown state -> state.Auth
-    | Known state   -> None
+    | Known state   -> Some d
     | _ -> None
 
 let openSession (state : KnownState) =
-    let k = state.Key
-    FreeDevice.Connected { Token = "test" }
+    let app_token = state.App_token
+    let rep = FreeBoxAPI.sendChallengeRequest
+    if rep.success then
+        printfn "Challenge reçu : %s" rep.result.challenge
+        printfn "Ouverture session..."
+        let session = FreeBoxAPI.sendOpenSessionRequest app_token rep.result.password_salt rep.result.challenge
+        if session.success then
+            printfn "Session ouverte, token : %s" session.result.session_token
+            Some (FreeDevice.Connected { Token = session.result.session_token })
+        else
+            None
+    else
+        None
 
 
 type KnownState with
     member this.OpenSession = openSession this
 
-// type NewState with
-//     member this.Register =
-
-// type ConnectedState with
-//     member this.GetFreeboxStats = "freebox stats"
-
-// let rec getFreeboxStats device =
-//     match device with
-//     | UnknownDevice state -> getFreeboxStats state.Start
-//     | NewDevice state     ->
-
-
-// type FreeDevice with
-//     member this.GetStats =
-
-// let callFree =
-//     let req = WebRequest.Create(Uri("http://mafreebox.freebox.fr/api_version"))
-//     use rep = req.GetResponse()
-//     use repStream = rep.GetResponseStream()
-//     use reader = new IO.StreamReader(repStream)
-//     reader.ReadToEnd()
+let connectionStatus (c : ConnectedState) =
+    let rep = FreeBoxAPI.sendInternetStatusRequest c.Token
+    if rep.success then
+        Some (String.Format("d: {0:###0}ko - u: {1:###0}ko" , rep.result.rate_down / 1000.0, rep.result.rate_up / 1000.0))
+    else
+        None
 
 [<EntryPoint>]
 let main argv =
     let device = startDevice
-    // match device with
-    // | Some d -> printfn "une clef %s" d
-    // | None   -> printfn "pas de clef"
+    match device with
+    | Some (Known s) ->
+        printfn "une clef %s" s.App_token
+        match s.OpenSession with
+        | Some (Connected s) ->
+            printfn "Connecté youhou !"
+            match connectionStatus s with
+            | Some d -> printfn "débits : %s" d
+            | _ -> printfn "aie"
+        | _ -> printfn "impossible de se connecter"
+    | _   -> printfn "Impossible de se connecter à la freebox"
 
 
     //let x = device.Register
     // match device.Register with
     // | Some k -> printfn "une clef %s" k
     // | None   -> printfn "pas de clef"
-    let rep = sendAuthorizeRequest "appid" "appname" "appversion" "appowner"
+    //let rep = sendAuthorizeRequest "appid" "appname" "appversion" "appowner"
 
-    printfn "%O" rep
+    //printfn "%O" rep
     0 // return an integer exit code
--- freeBoxAPI/FreeBoxAPI.fs
+++ freeBoxAPI/FreeBoxAPI.fs
@@ -2,6 +2,8 @@ module FreeBoxAPI
 
 open System
 open System.Net
+open System.IO
+open System.Text
 open Newtonsoft.Json
 
 type AuthResult = {
@@ -14,6 +16,76 @@ type AuthResponse = {
     result : AuthResult
 }
 
+type ChallengeResult = {
+    logged_in : bool
+    challenge : string
+    password_salt : string
+}
+
+type ChallengeResponse = {
+    success : bool
+    result : ChallengeResult
+}
+
+type OpenSessionResult= {
+    session_token : string
+    challenge : string
+}
+
+type OpenSessionResponse = {
+    success : bool
+    result : OpenSessionResult
+}
+
+type CheckStatusResult = {
+    status : string
+}
+
+type CheckStatusResponse = {
+    success : bool
+    result : CheckStatusResult
+}
+
+type InternetStatusResult = {
+    rate_down : float
+    rate_up : float
+}
+
+type InternetStatusResponse = {
+    success : bool
+    result : InternetStatusResult
+}
+
+let sendOpenSessionRequest (app_token : string) (password_salt : string) (challenge : string) : OpenSessionResponse =
+    printfn "app_token : <%s>  salt : <%s>   challenge : <%s>" app_token password_salt challenge
+    let hmac = new System.Security.Cryptography.HMACSHA1(Encoding.UTF8.GetBytes(app_token))
+    let pass = hmac.ComputeHash(Encoding.UTF8.GetBytes(challenge))
+                |> Seq.map (fun b -> b.ToString("x2"))
+                |> String.Concat
+    let client = new Http.HttpClient()
+    let contentPosted = (sprintf """{
+   "app_id": "fr.freebox.testapp",
+   "app_version": "0.0.7",
+   "password" : "%s"
+}""" pass)
+    let content = new Http.StringContent(contentPosted)
+    printfn "post data : %s" contentPosted
+    let rep = client.PostAsync(Uri("http://mafreebox.freebox.fr/api/v4/login/session"), content)
+    let json = rep.Result.Content.ReadAsStringAsync().Result
+    JsonConvert.DeserializeObject<OpenSessionResponse>(json);
+
+let sendChallengeRequest : ChallengeResponse =
+    let client = new Http.HttpClient()
+    let rep = client.GetAsync(Uri("http://mafreebox.freebox.fr/api/v4/login"))
+    let json = rep.Result.Content.ReadAsStringAsync().Result
+    JsonConvert.DeserializeObject<ChallengeResponse>(json);
+
+let sendCheckStatus track_id : CheckStatusResponse =
+    let client = new Http.HttpClient()
+    let rep = client.GetAsync(Uri(sprintf "http://mafreebox.freebox.fr/api/v4/login/authorize/%d" track_id))
+    let json = rep.Result.Content.ReadAsStringAsync().Result
+    JsonConvert.DeserializeObject<CheckStatusResponse>(json);
+
 let sendAuthorizeRequest (app_id : string) (app_name : string) (app_version : string) (device_name : string) : AuthResponse =
     let client = new Http.HttpClient()
     let content = new Http.StringContent("""{
@@ -25,3 +97,11 @@ let sendAuthorizeRequest (app_id : string) (app_name : string) (app_version : st
     let rep = client.PostAsync(Uri("http://mafreebox.freebox.fr/api/v4/login/authorize"), content)
     let json = rep.Result.Content.ReadAsStringAsync().Result
     JsonConvert.DeserializeObject<AuthResponse>(json);
+
+
+let sendInternetStatusRequest (session_token : string) : InternetStatusResponse =
+    let client = new Http.HttpClient()
+    client.DefaultRequestHeaders.Add("X-Fbx-App-Auth", session_token)
+    let rep = client.GetAsync(Uri("http://mafreebox.freebox.fr/api/v4/connection"))
+    let json = rep.Result.Content.ReadAsStringAsync().Result
+    JsonConvert.DeserializeObject<InternetStatusResponse>(json);
